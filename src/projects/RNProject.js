67d390ccb81b83b533bdffee35fcdf8ae8bd844c
Charles Park <charlieboyee@gmail.com>
2022-10-31 08:06:32 -0700

    Video playback buttons fixed. Left side title, title shadow and buttons aligned.

--- src/projects/RNProject.js
+++ src/projects/RNProject.js
@@ -1,28 +1,55 @@
+import { useRef } from 'react';
 import { Button, Typography } from '@mui/material';
 import './RNProject.css';
 import RNativeVid from '../assets/RNativeProject.mp4';
 
 const RNProject = () => {
+	const vidRef = useRef(null);
+
+	const playVid = () => {
+		console.log('play');
+		const myVid = vidRef.current;
+
+		if (myVid.paused) {
+			myVid.play();
+		}
+	};
+
+	const pauseVid = () => {
+		const myVid = vidRef.current;
+
+		if (!myVid.paused) {
+			myVid.pause();
+		}
+	};
+
 	return (
 		<>
 			<section className='left'>
-				<div>hello</div>
+				<Typography id='title' variant='h1'>
+					Photo Cacher
+				</Typography>
+				<div className='buttonsContainer'>
+					<Button variant='outlined'>Live Demo</Button>
+					<Button variant='outlined'>View Github</Button>
+				</div>
 			</section>
 
 			<section className='right'>
 				<div className='phone'>
 					<div className='panel front'>
-						<Typography id='title' color='common.white'>
-							Project title goes here
-						</Typography>
-
-						<video autoplay muted>
+						<Typography color='common.white'>Photo Cacher</Typography>
+						<div className='vidControls'>
+							<Button color='success' onClick={playVid}>
+								Play
+							</Button>
+							<Button color='warning' onClick={pauseVid}>
+								Pause
+							</Button>
+						</div>
+						<video ref={vidRef} autoPlay muted>
 							<source src={RNativeVid} type='video/mp4' />
 						</video>
-						<div className='vidControls'>
-							<Button color='success'>Play</Button>
-							<Button color='warning'>Pause</Button>
-						</div>
 					</div>
 					<div className='panel back'></div>
 				</div>
