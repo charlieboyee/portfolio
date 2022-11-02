85f074907c54f1291c2172cb306817b68fba3aa5
Charles Park <charlieboyee@gmail.com>
2022-11-02 08:46:51 -0700

    API fetch from github for languages used working.

--- src/About.js
+++ src/About.js
@@ -1,7 +1,68 @@
+import { useEffect, useState } from 'react';
 import { Card, CardContent, CardMedia, Paper, Typography } from '@mui/material';
 import NavBar from './NavBar';
 import './design/about.css';
+import { Octokit } from 'octokit';
+
 const About = () => {
+	const [languagesUsed, setLanguagesUsed] = useState(null);
+	const getLanguagesUsed = async () => {
+		try {
+			const result = await fetch(`${process.env.REACT_APP_URL}getOctoKitToken`);
+			if (result.status === 200) {
+				const { token } = await result.json();
+				const octokit = new Octokit({
+					auth: token,
+				});
+
+				const {
+					data: { login },
+				} = await octokit.rest.users.getAuthenticated();
+				const repos = await octokit.request('GET /user/repos', {});
+				const proms = repos.data?.map(async (repo, key) => {
+					if (repo.name === 'LETS') return;
+					try {
+						const { data } = await octokit.request(
+							'GET /repos/{owner}/{repo}/languages',
+							{
+								owner: login,
+								repo: repo?.name,
+							}
+						);
+						return data;
+					} catch (err) {
+						console.log(err);
+						return;
+					}
+				});
+
+				let countObj = {};
+
+				const langs = await Promise.all(proms);
+				langs.forEach((item, key) => {
+					if (item !== undefined) {
+						Object.keys(item).forEach((lang, k) => {
+							if (countObj[lang] === undefined) {
+								countObj[lang] = 0;
+							} else {
+								countObj[lang] += 1;
+							}
+						});
+					}
+				});
+				setLanguagesUsed(countObj);
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	useEffect(() => {
+		getLanguagesUsed();
+	}, []);
+
+	useEffect(() => {
+		console.log(languagesUsed);
+	}, [languagesUsed]);
 	return (
 		<Paper className='about'>
 			<NavBar />
