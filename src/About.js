1dc0b545837fa4ae746dc6eef24dbddcd36595fb
Charles Park <charlieboyee@gmail.com>
2022-11-02 11:20:42 -0700

    About page finished. Do some leet code and hackerrank to populate page...

--- src/About.js
+++ src/About.js
@@ -1,11 +1,26 @@
 import { useEffect, useState } from 'react';
 import { Card, CardContent, CardMedia, Paper, Typography } from '@mui/material';
 import NavBar from './NavBar';
-import './design/about.css';
 import { Octokit } from 'octokit';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+import { Pie } from 'react-chartjs-2';
+import './design/about.css';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const CustomCard = (props) => {
+	console.log(props);
+	return (
+		<Card className='cards' raised elevation={24} sx={props.sx}>
+			{props.children}
+		</Card>
+	);
+};
 
 const About = () => {
 	const [languagesUsed, setLanguagesUsed] = useState(null);
+
 	const getLanguagesUsed = async () => {
 		try {
 			const result = await fetch(`${process.env.REACT_APP_URL}getOctoKitToken`);
@@ -59,25 +74,110 @@ const About = () => {
 	useEffect(() => {
 		getLanguagesUsed();
 	}, []);
-
-	useEffect(() => {
-		console.log(languagesUsed);
-	}, [languagesUsed]);
 	return (
 		<Paper className='about'>
 			<NavBar />
-			<Typography>Hey there!</Typography>
-			<Typography>Here's a little bit more about me</Typography>
-			<Card>
-				<CardContent>
-					<Typography>Education</Typography>
-				</CardContent>
-			</Card>
-			{/* Education */}
-			{/* hobbies */}
-			{/* Age under 30 :] */}
-			{/* leet code problems completed */}
-			{/* hacker rank problems completed */}
+			<main>
+				<section className='left'>
+					<Typography variant='h2'>Education</Typography>
+					<CustomCard
+						sx={{ backgroundColor: 'primary.dark', color: 'common.white' }}
+					>
+						<CardContent>
+							<Typography variant='h4'>Coding Experience</Typography>
+							<Typography variant='h6' component='li'>
+								React
+							</Typography>
+							<Typography variant='h6' component='li'>
+								React Native w/ Expo
+							</Typography>
+							<Typography variant='h6' component='li'>
+								Auth Services (Firebase and some AWS)
+							</Typography>
+							<Typography variant='h6' component='li'>
+								SQL/PostgreSQL
+							</Typography>
+							<Typography variant='h6' component='li'>
+								Nodejs w/Express.js
+							</Typography>
+							<Typography variant='h6' component='li'>
+								JS, HTML, CSS (Material UI for design)
+							</Typography>
+						</CardContent>
+					</CustomCard>
+					<CustomCard
+						sx={{ backgroundColor: 'secondary.dark', color: 'common.white' }}
+					>
+						<CardContent>
+							<Typography variant='h4'>
+								CSUN (California State University of Northridge)
+							</Typography>
+
+							<Typography variant='h6' component='li'>
+								B.S. (Hospitality & Recreation Management)
+							</Typography>
+						</CardContent>
+					</CustomCard>
+					<Typography variant='h2'>Interests</Typography>
+
+					<CustomCard
+						sx={{ backgroundColor: 'secondary.main', color: 'common.white' }}
+					>
+						<CardContent>
+							<Typography variant='h6' component='li'>
+								Cars
+							</Typography>
+							<Typography variant='h6' component='li'>
+								Photography
+							</Typography>
+							<Typography variant='h6' component='li'>
+								Hiking
+							</Typography>
+							<Typography variant='h6' component='li'>
+								Biking
+							</Typography>
+						</CardContent>
+					</CustomCard>
+				</section>
+				<section className='right'>
+					{/* fill this are with leet code and hacker rank question stats */}
+					{languagesUsed && (
+						<div id='pieContainer'>
+							<Pie
+								data={{
+									labels: Object.keys(languagesUsed),
+									datasets: [
+										{
+											label: 'Languages Used',
+											data: Object.values(languagesUsed),
+											backgroundColor: [
+												'rgba(255, 99, 132, 0.2)',
+												'rgba(54, 162, 235, 0.2)',
+												'rgba(255, 206, 86, 0.2)',
+												'rgba(75, 192, 192, 0.2)',
+												'rgba(153, 102, 255, 0.2)',
+												'rgba(255, 159, 64, 0.2)',
+											],
+											borderColor: [
+												'rgba(255, 99, 132, 1)',
+												'rgba(54, 162, 235, 1)',
+												'rgba(255, 206, 86, 1)',
+												'rgba(75, 192, 192, 1)',
+												'rgba(153, 102, 255, 1)',
+												'rgba(255, 159, 64, 1)',
+											],
+											borderWidth: 3,
+										},
+									],
+								}}
+							/>
+							<Typography variant='caption' color='grey'>
+								Data from personal github
+							</Typography>
+						</div>
+					)}
+				</section>
+			</main>
 		</Paper>
 	);
 };
--- src/theme.js
+++ src/theme.js
@@ -24,15 +24,15 @@ export const getTheme = (mode) => ({
 			? {
 					primary: { main: '#FF0000' },
 					secondary: { main: '#1474F9' },
-					text: { primary: '#ffffff' },
-					common: { black: '#000000', white: '#f5f5f5' },
+					text: { primary: '#ffffff', secondary: '#000000' },
+					common: { black: '#000000', white: '#ffffff' },
 					background: { paper: '#1a1a1a' },
 			  }
 			: {
 					primary: { main: '#1474F9' },
-					// secondary: { main: '#FF7F00' },
-					text: { primary: '#000000' },
-					common: { black: '#000000', white: '#f5f5f5' },
+					secondary: { main: '#FF7F00' },
+					text: { primary: '#000000', secondary: '#ffffff' },
+					common: { black: '#000000', white: '#ffffff' },
 					background: { paper: '#CFE2D3' },
 			  }),
 	},
