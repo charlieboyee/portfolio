546b1d20e2c0ec93654e5011d2acfd70eaf64008
Charles Park <charlieboyee@gmail.com>
2022-11-03 04:31:59 -0700

    maintnenace mode.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import { Typography } from '@mui/material';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<RouterProvider router={router} />);
+root.render(
+	<Typography variant='h1' sx={{ textAlign: 'center' }}>
+		The website is under maintenance, plesase try again later. Thank you!
+	</Typography>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
